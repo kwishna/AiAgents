d10e43e68636a47d89c7cbdf629861f6c9113967
kwishna <kwishna@gmail.com>
2024-11-03 18:21:30 +0000

    function - math utils use in another script

---
+++ learn-bash/functions/math-utils-test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+. ./learn-bash/functions/math-utils.sh #This is just inclusion and we are not executing util.sh
+add 10 20
+subtract 20 10
+multiply 10 20
+divide 20 10
+echo "The value of x: $x"
+echo "The value of y: $y"
