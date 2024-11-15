b78786fb45e656fd50d393808034ae3529600ac9
kwishna <kwishna@gmail.com>
2024-11-15 01:19:22 +0000

    prompt file & read

---
+++ learn-bash/read-file-prompt.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+read -p "Enter any File name to display its content: " fname
+echo "-----------------------------------------------"
+cat $fname
+echo "-----------------------------------------------"
